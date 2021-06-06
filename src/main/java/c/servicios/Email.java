d7c49b91947003bd9551819f728888f3042a4d12
Daiana <alonsodaiana5@gmail.com>
2021-06-05 23:01:46 -0300

    Final

--- src/main/java/c/servicios/Email.java
+++ src/main/java/c/servicios/Email.java
@@ -9,17 +9,18 @@ import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class Email implements ServicioEmail {
-    private String remitente = "YPF@example.com";
+    private String REMITENTE = "YPF@example.com";
+    private final String USERNAME = "3660538112c0df";
+    private final String PASSWORD = "6946035db2f04d";
+    private String HOST = "smtp.mailtrap.io";
     private String destinatario;
-    private final String username = "3660538112c0df";
-    private final String password = "6946035db2f04d";
-    private String host = "smtp.mailtrap.io";
-    private Properties props = new Properties();
+    private Properties props;
 
     public Email() {
+        this.props =  new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");// it’s optional in Mailtrap
-        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.host", HOST);
         props.put("mail.smtp.port", "2525");
     }
 
@@ -31,12 +32,12 @@ public class Email implements ServicioEmail {
                 new javax.mail.Authenticator() {
                     @Override
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
+                        return new PasswordAuthentication(USERNAME, PASSWORD);
                     }
                 });
         try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(remitente));
+            message.setFrom(new InternetAddress(REMITENTE));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(destinatario));
             message.setSubject("Se realizó la carga de combustible de manera exitosa.");
